d5ac3ddd1cbe21dc4a7f24b4ebb7f839e047d3b3
Mark <mark@seriti.com>
2020-10-09 14:05:45 +0200

    document move action fix

--- Bucket/BucketFile.php
+++ Bucket/BucketFile.php
@@ -13,7 +13,8 @@ class BucketFile extends Upload
         $param = ['row_name'=>'Bucket document',
                   'pop_up'=>true,
                   'update_calling_page'=>true,
-                  'prefix'=>'']; //will prefix file_name if used, but file_id.ext is unique 
+                  'prefix'=>'',//will prefix file_name if used, but file_id.ext is unique 
+                  'upload_location'=>$id_prefix]; 
         parent::setup($param);
 
         if($this->user_access_level !== 'GOD' and $this->user_access_level !== 'ADMIN') {
